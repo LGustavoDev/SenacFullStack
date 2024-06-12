1b3816412bad843daf7234ca03883caa0384854c
Luiz Gustavo <151104065+LGustavoDev@users.noreply.github.com>
2024-06-11 21:09:39 -0300

    1

---
+++ FrontEnd/aula02/src/App.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="conteiner">
+
+      <div className='pai1'>
+
+        <div className='f1'></div>
+        <div className='f2'></div>
+
+
+        <div className='f3'>
+          <div className='n1'></div>
+          <div className='n2'></div>
+        </div>
+
+        <div className='f4'>
+        <div className='n3'></div>
+        <div className='n4'></div>
+        </div>
+
+
+        <div className='f5'></div>
+        <div className='f6'></div>
+
+      </div>
+
+
+
+
+
+
+
+      
+    </div>
+  );
+}
+
+export default App;
