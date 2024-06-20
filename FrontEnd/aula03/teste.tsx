3ab704e5038cdee943ed8c6323edcc1db64a6753
Luiz Gustavo <151104065+LGustavoDev@users.noreply.github.com>
2024-06-19 22:17:59 -0300

    atualizado

---
+++ FrontEnd/aula03/teste.tsx
@@ -0,0 +1,39 @@
+<div className='flexconteiner'>
+
+                <div className='div1'> </div>
+
+                <div className='div2'>
+                    <div className='pa'></div>
+                </div>
+
+                <div className='div3'></div >
+
+            </div>
+
+            <div className='pai2'>
+
+                <div className='div4'>
+
+                </div>
+
+                <div className='div5'>
+
+                </div>
+
+            </div>
+
+            <div className='pai3'>
+
+                <div className='div6'>
+
+                </div>
+
+                <div className='div7'>
+
+                </div>
+
+                <div className='div8'>
+
+                </div>
+
+            </div> }
--- FrontEnd/teste1/src/App.tsx
+++ FrontEnd/teste1/src/App.tsx
@@ -3,51 +3,22 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div>
-        <div className='flexconteiner'>
+    let num1: number = 10
+    let num2: number = 15
+    function somar(n1:number, n2:number): number {
+        return n1 + n2
 
-            <div className='div1'> </div>
+    }
+    return (
+        <div>
+            {somar(num1, num2)}
 
-            <div className='div2'>
-                <div className='pa'></div>
-            </div>     
 
-            <div className='div3'></div >
+
             
         </div>
 
-        <div className='pai2'>
-
-            <div className='div4'>
-
-            </div>
-
-            <div className='div5'>
-
-            </div>
-
-        </div>
-
-        <div className='pai3'>
-
-            <div className='div6'>
-
-            </div>
-
-            <div className='div7'>
-
-            </div>
-
-            <div className='div8'>
-
-            </div>
-
-        </div>
-        
-    </div>
-
-  );
+    );
 }
 
 export default App;
